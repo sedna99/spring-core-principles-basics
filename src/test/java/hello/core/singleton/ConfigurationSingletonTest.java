ebe6bd23cb1f29d0ba63c87e2b4836e1f0f09b8d
sedna99 <ljsmmm1228@gmail.com>
2024-05-07 23:40:03 +0900

    섹션5: @Configuration과 싱글톤

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean(MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository3 = ac.getBean(MemberRepository.class);
+
+        MemberRepository memberRepository1 = orderService.getMemberRepository();
+        MemberRepository memberRepository2 = memberService.getMemberRepository();
+
+        System.out.println("memberRepository1 = " + memberRepository1);
+        System.out.println("memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository3 = " + memberRepository3);
+
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository2);
+        Assertions.assertThat(memberRepository2).isSameAs(memberRepository3);
+
+    }
+}
