9d54a1ca0bcda4e14f81524190df4f0b19e7ba28
sedna99 <ljsmmm1228@gmail.com>
2024-05-24 23:24:10 +0900

    섹션9: request 스코프 예제 만들기

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,30 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.Setter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+    private String uuid;
+    @Setter
+    private String requestURL;
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "][" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = java.util.UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean created: " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean closed: " + this);
+    }
+}
